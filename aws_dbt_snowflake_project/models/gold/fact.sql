95ab3c19ee5a0b5fcf6232cca102ca43f9661451
Tanay Jujarao <tanayjujarao@gmail.com>
2026-08-14 22:39:23 +0530

    project done

---
+++ aws_dbt_snowflake_project/models/gold/fact.sql
@@ -0,0 +1,52 @@
+{% set congigs = [
+    {
+        "table" : "AIRBNB.GOLD.OBT",
+        "columns" : "GOLD_obt.BOOKING_ID, GOLD_obt.LISTING_ID, GOLD_obt.HOST_ID,GOLD_obt.TOTAL_AMOUNT, GOLD_obt.SERVICE_FEE, GOLD_obt.CLEANING_FEE, GOLD_obt.ACCOMMODATES, GOLD_obt.BEDROOMS, GOLD_obt.BATHROOMS, GOLD_obt.PRICE_PER_NIGHT, GOLD_obt.RESPONSE_RATE",
+        "alias" : "GOLD_obt"
+    },
+    { 
+        "table" : "AIRBNB.GOLD.DIM_LISTINGS",
+        "columns" : "",
+        "alias" : "DIM_listings",
+        "join_condition" : "GOLD_obt.listing_id = DIM_listings.listing_id"
+    },
+    {
+        "table" : "AIRBNB.GOLD.DIM_HOSTS",
+        "columns" : "",
+        "alias" : "DIM_hosts",
+        "join_condition" : "GOLD_obt.host_id = DIM_hosts.host_id"
+    }
+] %}
+
+
+WITH FACT_CTE AS (
+SELECT 
+        {{ congigs[0]['columns'] }}
+
+FROM
+    {% for config in congigs %}
+    {% if loop.first %}
+      {{ config['table'] }} AS {{ config['alias'] }}
+    {% else %}
+        LEFT JOIN {{ config['table'] }} AS {{ config['alias'] }}
+        ON {{ config['join_condition'] }}
+        {% endif %}
+        {% endfor %}
+
+
+)
+
+SELECT
+    DISTINCT BOOKING_ID,
+    LISTING_ID,
+    HOST_ID,
+    TOTAL_AMOUNT,
+    SERVICE_FEE,
+    CLEANING_FEE,
+    ACCOMMODATES,
+    BEDROOMS,
+    BATHROOMS,
+    PRICE_PER_NIGHT,
+    RESPONSE_RATE
+FROM 
+    FACT_CTE
